728144c1ed896c1470fea87d919b8a17f3bcf63f
João Lucas Barreto <141835071+Joaozin148@users.noreply.github.com>
2025-04-22 17:52:42 -0300

    Criação do index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include("includes/header.php"); ?>
+
+<section class="conteudo">
+    <h2>Bem-vindo ao PetShop Feliz!</h2>
+    <p>Cuidados e carinho para seu animalzinho de estimação.</p>
+</section>
+
+<?php include("includes/footer.php"); ?>
